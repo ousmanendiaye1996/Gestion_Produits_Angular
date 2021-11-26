b43a87428b569d2a601fcaa808c7760fb71cb71a
ousmane_ndiaye <ousmane.ndiaye.dev@gmail.com>
2021-11-26 18:27:19 +0000

    Mon premiere commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
+
+import { UpdateProduitComponent } from './update-produit/update-produit.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+            declarations: [
+              AppComponent,
+              ProduitsComponent,
+              AddProduitComponent,
+             UpdateProduitComponent
+            ],
+            imports: [
+              BrowserModule,
+              AppRoutingModule,
+              
+             FormsModule,
+             ReactiveFormsModule
+
+            ],
+            providers: [],
+            bootstrap: [AppComponent]
+        })
+
 export class AppModule { }
